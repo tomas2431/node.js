a12cc33473db1767f2dab021009fced427a446ea
tomas2431 <107480819+tomas2431@users.noreply.github.com>
2022-06-30 16:56:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const https = require('https');
-const port = 80;
+const port = 443;
 
-const server = http.createServer((req,res) => {
+const server = https.createServer((req,res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello world');
